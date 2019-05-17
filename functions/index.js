165dc5d36c96a7b6f7efdd103e1df7b4a341daf4
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-05-17 17:08:56 +0900

    Implement a sample with the axios lib

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,24 @@
 'use strict';
 
 exports.helloWorld = (req, res) => {
+    console.log('in hello world.');
     res.send('Hello World!' + ', env:' + process.env.SAMPLE);
+};
+
+const axios = require('axios');
+const axiosClient = axios.create({
+    baseURL: process.env.API_ENDPOINT,
+    responseType: 'json'
+});
+
+exports.listingApi = (req, res) => {
+    console.log("Called listing api.");
+
+    axiosClient.get('/challeweb/json/articles.json')
+        .then(function (response) {
+            console.log('get articles');
+            console.log(response.data);
+        });
+
+    res.status(204).send('');
 };
